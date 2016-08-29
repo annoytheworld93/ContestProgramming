c389c06bebd850fd92bf6dab17ff358dc52d468a
Will <williamparks94@gmail.com>
2016-08-28 21:35:17 -0400

    Added Euler's Algorihm

---
+++ Problems/src/FloodFill.java
@@ -0,0 +1,35 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by wap5053 on 9/2/16.
+ */
+public class FloodFill {
+    class Cell{
+        int row, col;
+        public Cell(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public static void main(String[] args){
+
+    }
+
+    public static void floodFill(){
+        char[][] map = new char[][];
+        //TODO Read vales into map
+        int startRow, startCol;
+
+        Queue<Cell> queue = new LinkedList<>();
+        int cellCount = 1;
+        queue.add(new Cell(startRow, startCol));
+        while (){
+            Cell cell = queue.remove();
+            /*
+            for each neighbor n of cell do:
+                if(map
+             */
+        }
+    }
+}
