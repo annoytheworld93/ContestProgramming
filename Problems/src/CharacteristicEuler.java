a4abce410a32c293b8dfa132d6298494cefb2780
Will <williamparks94@gmail.com>
2016-09-15 20:01:29 -0400

    Update CharacteristicEuler.java

--- Problems/src/CharacteristicEuler.java
+++ Problems/src/CharacteristicEuler.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Created by Alexander on 9/15/2016.
- */
+
+
+
 public class CharacteristicEuler {
 
 	static int shadedRegions = 0;
@@ -25,95 +25,108 @@ public class CharacteristicEuler {
 			for (int i = 0; i < height; i++) {
 				map[i] = scanner.nextLine().toCharArray();
 			}
-			//System.out.println(Arrays.deepToString(map));
-			//System.out.println(map[height - 1][width - 1]);
+			char[][] bigMap = new char[height+2][width+2];
+			for(int i = 0; i< height+2; i++)
+				for(int k = 0; k< width+2; k++ )
+					bigMap[i][k]='O';
+			for(int i = 1; i< height+1; i++)
+				for(int k = 1; k< width+1; k++)
+				{
+					bigMap[i][k] = map[i-1][k-1];
+				}
+
+			System.out.println(Arrays.deepToString(bigMap));
+//System.out.println(map[height - 1][width - 1]);
+			debug(map);
 			floodFillO(0, 0, 'O', '*', map);
+			debug(map);
 			floodFillStar(0, 0, '*', '@', map);
-			//map[0][0] = '*';
-			//System.out.println(Arrays.deepToString(map));
-			//System.out.println("Shaded Regions: " + shadedRegions);
-			//System.out.println("Hole Counter: " + holeCounter);
+			debug(map);
+//map[0][0] = '*';
+//System.out.println(Arrays.deepToString(map));
+			System.out.println("Shaded Regions: " + shadedRegions);
+			System.out.println("Hole Counter: " + holeCounter);
 			System.out.println((shadedRegions - holeCounter));
 		}
 	}
 
-	public static void floodFillO(int x, int y, char oldChar, char newChar, char[][] map) {
+	public static void floodFillO(int x, int y, char oldChar, char newChar, char[][] bigMap) {
 //TODO Check left, right, up, and down for start
 //Base Cases, if we are out of index, i.e. less than one
 		if (x < 0) return;
 		if (y < 0) return;
-		if (x >= map.length) return;
-		if (y >= map[x].length) return;
+		if (x >= bigMap.length) return;
+		if (y >= bigMap[x].length) return;
 
 	//oldChar = map[x][y];
 
 
-		if (newChar == map[x][y]) return;
+		if (newChar == bigMap[x][y]) return;
 
 		//if (oldChar != map[x][y]) return;
 
-		if (map[x][y] == 'X'){
+		if (bigMap[x][y] == 'X'){
 			shadedRegions++;
-			floodFillX(x, y, 'X', '*', map);
+			floodFillX(x, y, 'X', '*', bigMap);
 		}
-		if (map[x][y] == oldChar) {
-			map[x][y] = newChar;
+		if (bigMap[x][y] == oldChar) {
+			bigMap[x][y] = newChar;
 
-			floodFillO(x - 1, y, oldChar, newChar, map);
-			floodFillO(x + 1, y, oldChar, newChar, map);
-			floodFillO(x, y - 1, oldChar, newChar, map);
-			floodFillO(x, y + 1, oldChar, newChar, map);
+			floodFillO(x - 1, y, oldChar, newChar, bigMap);
+			floodFillO(x + 1, y, oldChar, newChar, bigMap);
+			floodFillO(x, y - 1, oldChar, newChar, bigMap);
+			floodFillO(x, y + 1, oldChar, newChar, bigMap);
 		}
 	}
 
-	public static void floodFillX(int x, int y, char oldChar, char newChar, char[][]map){
+	public static void floodFillX(int x, int y, char oldChar, char newChar, char[][] bigMap){
 //TODO Check, left, right, up, down, and diagonals for start
 //Base Cases, if we are out of index, i.e. less than one
 		if (x < 0) return;
 		if (y < 0) return;
-		if (x >= map.length) return;
-		if (y >= map[x].length) return;
+		if (x >= bigMap.length) return;
+		if (y >= bigMap[x].length) return;
 
 
-		if (newChar == map[x][y]) return;
-		if (oldChar != map[x][y]) return;
+		if (newChar == bigMap[x][y]) return;
+		if (oldChar != bigMap[x][y]) return;
 
-		map[x][y]= newChar;
+		bigMap[x][y]= newChar;
 //Cross Shape
-			floodFillX(x - 1, y, oldChar, newChar, map);
-			floodFillX(x + 1, y, oldChar, newChar, map);
-			floodFillX(x, y - 1, oldChar, newChar, map);
-			floodFillX(x, y + 1, oldChar, newChar, map);
+			floodFillX(x - 1, y, oldChar, newChar, bigMap);
+			floodFillX(x + 1, y, oldChar, newChar, bigMap);
+			floodFillX(x, y - 1, oldChar, newChar, bigMap);
+			floodFillX(x, y + 1, oldChar, newChar, bigMap);
 //Diagonals
-			floodFillX(x - 1, y + 1, oldChar, newChar, map);
-			floodFillX(x + 1, y + 1, oldChar, newChar, map);
-			floodFillX(x - 1, y - 1, oldChar, newChar, map);
-			floodFillX(x + 1, y - 1, oldChar, newChar, map);
+			floodFillX(x - 1, y + 1, oldChar, newChar, bigMap);
+			floodFillX(x + 1, y + 1, oldChar, newChar, bigMap);
+			floodFillX(x - 1, y - 1, oldChar, newChar, bigMap);
+			floodFillX(x + 1, y - 1, oldChar, newChar, bigMap);
 	}
 
-	public static void floodFillStar(int x, int y, char oldChar, char newChar, char[][] map){
+	public static void floodFillStar(int x, int y, char oldChar, char newChar, char[][] bigMap){
 		if (x < 0) return;
 		if (y < 0) return;
-		if (x >= map.length) return;
-		if (y >= map[x].length) return;
+		if (x >= bigMap.length) return;
+		if (y >= bigMap[x].length) return;
 
-		if (newChar == map[x][y]) return;
+		if (newChar == bigMap[x][y]) return;
 		//if (oldChar != map[x][y]) return;
 
-		if (map[x][y] == 'O'){
+		if (bigMap[x][y] == 'O'){
 			holeCounter++;
 			//FIXME
-			floodFillO(x, y, 'O', '@', map);
+			floodFillO(x, y, 'O', '@', bigMap);
 		}
 
-		map[x][y] = newChar;
+		bigMap[x][y] = newChar;
 
 
 		//TODO Need to call floodFillO and then increment hole counter if we encounter an O!!!!
-		floodFillStar(x - 1, y, oldChar, newChar, map);
-		floodFillStar(x + 1, y, oldChar, newChar, map);
-		floodFillStar(x, y - 1, oldChar, newChar, map);
-		floodFillStar(x, y + 1, oldChar, newChar, map);
+		floodFillStar(x - 1, y, oldChar, newChar, bigMap);
+		floodFillStar(x + 1, y, oldChar, newChar, bigMap);
+		floodFillStar(x, y - 1, oldChar, newChar, bigMap);
+		floodFillStar(x, y + 1, oldChar, newChar, bigMap);
 
 	}
 
