47b3c1fcc0b519a1f367e595adca132152fce9c9
Will <williamparks94@gmail.com>
2016-12-03 02:28:37 -0500

    finished SecurePasswords

--- Problems/src/SecurePasswords.java
+++ Problems/src/SecurePasswords.java
@@ -10,12 +10,17 @@ public class SecurePasswords {
 		int numPasswords = scanner.nextInt();
 		for (int i = 0; i < numPasswords; i ++){
 			String testPassword = scanner.nextLine();
-			if (testPassword.length() < 8){notSecure(testPassword);}
-			else if (testPassword.length() > 24){notSecure(testPassword);}
-			else if (!testPassword.contains(/*a lower case letter*/)){notSecure(testPassword);}
-			else if (!testPassword.contains(/*an upper case letter*/)){notSecure(testPassword);}
-			else if (!testPassword.contains((/*a digit or a special character*/))){notSecure();}
-			else{secure(testPassword);
+			boolean isAtLeast8 = testPassword.length() >= 8; //Checks to see if it least 8 characters
+			boolean isNoMoreThan24 = testPassword.length() <= 24; //Checks to see if it less than 25 characters
+			boolean hasUpperCase = !testPassword.equals(testPassword.toUpperCase());
+			boolean hasLowerCase = !testPassword.equals(testPassword.toLowerCase());
+			boolean hasSpecial = !testPassword.matches("[0-9!@#$%^&*]");
+			if (isAtLeast8 && hasUpperCase && isNoMoreThan24 && hasLowerCase && hasSpecial){
+				secure(testPassword);
+			}
+			else {
+				notSecure(testPassword);
+			}
 		}
 	}
 
