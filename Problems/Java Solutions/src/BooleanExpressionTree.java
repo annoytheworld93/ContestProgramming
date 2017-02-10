13c08ccb57570f57b9204ac7a08e8d99143f1d4c
Will <williamparks94@gmail.com>
2017-02-10 15:09:48 -0500

    Add files via upload

---
+++ Problems/Java Solutions/src/BooleanExpressionTree.java
@@ -0,0 +1,14 @@
+/**
+* @author Will Parks
+* Contest Programming 
+* Boolean Tree Expression
+*/
+
+
+class BooleanExpressionTree {
+	
+	static void main(String[] args){
+		
+	}
+	
+}
---
+++ Problems/Java Solutions/src/Water.java
@@ -0,0 +1,22 @@
+//Water is your new best friend
+
+
+//inner class
+
+class Water{
+	class Location{
+		int r, c;
+		Location(int r, int c)}
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	//How do you do this with a Queue?????
+	public static void main(String[] args){
+	//TODO: Get that input!
+	
+	}
+	
+	
+}
